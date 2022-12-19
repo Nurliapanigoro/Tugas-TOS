80ed14aeb2d01d2c52a2011e9c460e46eeab9570
Herdin22 <120780477+Herdin22@users.noreply.github.com>
2022-12-19 15:50:41 +0800

    menambah langkah 3
    Disini saya menambah langkah 3 pada index.java

--- index.js
+++ index.js
@@ -25,3 +25,34 @@ class SegitigaAngka {
      
   }
 }
+
+// langkah 3
+
+import java.util.Scanner;
+   
+class SegitigaAngka {
+  public static void main(String args[]){
+            
+    Scanner input = new Scanner(System.in);
+       
+    System.out.println("##  Program Java Segitiga Angka  ##");
+    System.out.println("===================================");
+    System.out.println();
+     
+    int tinggi_segitiga,i,j,k = 0;
+  
+    System.out.print("Input tinggi segitiga: ");
+    tinggi_segitiga = input.nextInt();
+     
+    System.out.println();
+    
+    for(i=1; i<=tinggi_segitiga; i++) {
+      for(j=1; j<=i; j++) {
+        k++;
+        System.out.print(k+" ");
+      }
+      System.out.println();
+    }
+     
+  }
+}
